e896d2d3dceefdcefea8e1ba56a324ce121ddbd5
Prashant Ghimire <ghimire.prashantg@gmail.com>
2022-03-05 23:07:45 +0545

    new data_dividend_file

---
+++ data_dividend_2.sql
@@ -0,0 +1,16 @@
+insert into 
+	dividend (company, fiscal_year)
+	
+values
+('AHPC',20702071),
+('AHPC',20712072),
+('AHPC',20732074),
+('AHPC',20762077),
+('CZBIL',20692070),
+('CZBIL',20702071),
+('CZBIL',20712072),
+('CZBIL',20732074),
+('GBIME',20692070),
+('GBIME',20702071),
+('GBIME',20712072),
+('GBIME',20732074);
